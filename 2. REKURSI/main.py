dcd585b5abf07a9b3497bff8b24274c2be3324db
GilangPradnyana <gilangpradnyana2@gmail.com>
2026-03-13 14:53:45 +0800

    Add new Folder

---
+++ 2. REKURSI/main.py
@@ -0,0 +1,16 @@
+# 1. Most common style
+print("Hello World")
+
+# 2. With an exclamation mark (your version)
+print("Hello world!")
+
+# 3. With single quotes (also correct)
+print('Hello World')
+
+# 4. With a variable (very common when learning)
+message = "Hello World"
+print(message)
+
+# 5. Python 3.6+ f-string style (modern & clean)
+name = "Gilang"
+print(f"Hello World from {name}!")
