1ab1593bd694e1415738f7ecf992f752921f11f7
GilangPradnyana <roronoazoro07xy @gmail.com>
2026-04-17 12:16:53 +0800

    Add new UTS File

---
+++ UTS/index.py
@@ -0,0 +1,5 @@
+# =====================================================
+# SISTEM ANTRIAN PRINTER - Menggunakan Queue + Linked List
+# Project UTS Struktur Data - Semester Genap 2025/2026
+# Studi Kasus: Sistem Antrian Printer di Laboratorium Komputer
+# =====================================================
